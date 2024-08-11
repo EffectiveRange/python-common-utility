76583316eef33406c2aa1e31125dcfd7a9b99f96
AttilaGombosER <attila.gombos@effective-range.com>
2024-08-11 17:33:50 +0200

    Cleanup (#3)

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name="python-common-utility",
-    version="0.1.0",
-    description="Common utility packages for Python projects",
-    author="Ferenc Nandor Janky & Attila Gombos",
-    author_email="info@effective-range.com",
-    packages=["common_utility", "test_utility"],
-    package_data={"common_utility": ["py.typed"]},
+    name='python-common-utility',
+    version='0.1.0',
+    description='Common utility packages for Python projects',
+    author='Ferenc Nandor Janky & Attila Gombos',
+    author_email='info@effective-range.com',
+    packages=['common_utility', 'test_utility'],
+    package_data={'common_utility': ['py.typed'], 'test_utility': ['py.typed']},
     install_requires=[
-        "requests",
-        "pydantic",
-        "jinja2",
-        "python-context-logger@git+https://github.com/EffectiveRange/python-context-logger.git@latest",
+        'requests',
+        'pydantic',
+        'jinja2',
+        'python-context-logger@git+https://github.com/EffectiveRange/python-context-logger.git@latest',
     ],
 )
