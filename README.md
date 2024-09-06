8b4bf52d1c1333a1cb81161784bf8b29bc7c3a0f
Attila Gombos <attila.gombos@effective-range.com>
2024-09-06 13:19:11 +0200

    Update and unit test reusable timer

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 [![Test and Release](https://github.com/EffectiveRange/python-common-utility/actions/workflows/test_and_release.yml/badge.svg)](https://github.com/EffectiveRange/python-common-utility/actions/workflows/test_and_release.yml)
 [![Coverage badge](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/EffectiveRange/python-common-utility/python-coverage-comment-action-data/endpoint.json)](https://htmlpreview.github.io/?https://github.com/EffectiveRange/python-common-utility/blob/python-coverage-comment-action-data/htmlcov/index.html)
 
--- common_utility/reusableTimer.py
+++ common_utility/reusableTimer.py
@@ -5,11 +5,19 @@
 from threading import Timer, Lock
 from typing import Any, Iterable, Optional, Mapping
 
+from context_logger import get_logger
+
+log = get_logger('ReusableTimer')
+
 
 class IReusableTimer(object):
 
     def start(
-        self, interval_sec: int, function: Any, *args: Optional[Iterable[Any]], **kwargs: Optional[Mapping[str, Any]]
+        self,
+        interval: float,
+        function: Any,
+        args: Optional[Iterable[Any]] = None,
+        kwargs: Optional[Mapping[str, Any]] = None,
     ) -> 'IReusableTimer':
         raise NotImplementedError()
 
@@ -30,18 +38,31 @@ class ReusableTimer(IReusableTimer):
         self._timer: Optional[Timer] = None
 
     def start(
-        self, interval_sec: int, function: Any, *args: Optional[Iterable[Any]], **kwargs: Optional[Mapping[str, Any]]
+        self,
+        interval: float,
+        function: Any,
+        args: Optional[Iterable[Any]] = None,
+        kwargs: Optional[Mapping[str, Any]] = None,
     ) -> IReusableTimer:
         with self._timer_lock:
             if self._timer:
                 self._timer.cancel()
-            self._timer = Timer(interval_sec, function, args, kwargs)
+
+            log.debug('Starting timer', interval=interval, function=function, args=args, kwargs=kwargs)
+            self._timer = Timer(interval, function, args, kwargs)
             self._timer.start()
         return self
 
     def restart(self) -> None:
         with self._timer_lock:
             if self._timer:
+                log.debug(
+                    'Restarting timer',
+                    interval=self._timer.interval,
+                    function=self._timer.function,
+                    args=self._timer.args,
+                    kwargs=self._timer.kwargs,
+                )
                 self._timer.cancel()
                 self._timer = Timer(self._timer.interval, self._timer.function, self._timer.args, self._timer.kwargs)
                 self._timer.start()
@@ -49,6 +70,13 @@ class ReusableTimer(IReusableTimer):
     def cancel(self) -> None:
         with self._timer_lock:
             if self._timer:
+                log.debug(
+                    'Cancelling timer',
+                    interval=self._timer.interval,
+                    function=self._timer.function,
+                    args=self._timer.args,
+                    kwargs=self._timer.kwargs,
+                )
                 self._timer.cancel()
                 self._timer = None
 
