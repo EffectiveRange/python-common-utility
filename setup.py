fd2385447f40dd4b0e7b641fa6fee1922ee4a312
GitHub Action <action@github.com>
2024-09-13 08:49:50 +0000

    Update version to 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-common-utility',
-    version='1.0.2',
+    version='1.0.3',
     description='Common utility packages for Python projects',
     author='Ferenc Nandor Janky & Attila Gombos',
     author_email='info@effective-range.com',
